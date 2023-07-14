7b326621c4861c5aa33d9503885ae89c83b685fd
MOURIG ABDELLAH <mourig.abdellah.dev@gmail.com>
2023-07-14 16:49:40 +0200

    better

--- app.py
+++ app.py
@@ -1,37 +1,113 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pytrends.request import TrendReq
 import pandas as pd
+import re
+import random
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+MAX_CONNECTIONS = 5
+
+class TrendReqPool:
+    def __init__(self):
+        self.connection_pool = []
+
+    def get_connection(self):
+        if not self.connection_pool:
+            pytrends = TrendReq(hl='en-US', tz=360)
+            return pytrends
+        else:
+            return self.connection_pool.pop()
+
+    def return_connection(self, pytrends):
+        self.connection_pool.append(pytrends)
+
+trendreq_pool = TrendReqPool()
+
+def extract_arrays(list_of_items):
+  """Extracts the arrays and their values from a list and returns a new list with the arrays as separate items.
+
+  Args:
+    list_of_items: A list of items, some of which are arrays.
+
+  Returns:
+    A new list with the arrays as separate items.
+  """
+
+  new_list = []
+  for item in list_of_items:
+    if isinstance(item, list):
+      new_list.extend(item)
+    elif "," not in item:
+      new_list.append(item)
+
+  return new_list
+
+def remove_non_english(data):
+  """Removes all non-English characters from a string.
+
+  Args:
+    data: The string to be modified.
+
+  Returns:
+    The modified string.
+  """
+
+  non_english_regex = re.compile(r'[^\x00-\x7F]')
+  modified_data = non_english_regex.sub('', data)
+  return modified_data
+
+
+@cache.memoize(timeout=60*60)  # Cache the response for 1 hour
+def fetch_suggestions(keyword, pytrends):
+    return pytrends.suggestions(keyword=keyword)
 
 @app.route('/', methods=['GET'])
 def get_search_trends():
-    # Set up pytrends
-    pytrends = TrendReq(hl='en-US', tz=360)
+    pytrends_pool = trendreq_pool
 
-    # Get daily search trends for the United States
-    trending_searches_df = pytrends.trending_searches(pn='united_states')
+    with ThreadPoolExecutor(max_workers=MAX_CONNECTIONS) as executor:
+        pytrends = pytrends_pool.get_connection()
 
-    # Process the retrieved data
-    trending_searches = trending_searches_df.values.flatten().tolist()
+        trending_searches_df = pytrends.trending_searches(pn='united_states')
+        trending_searches_df_canada = pytrends.trending_searches(pn='canada')
+        realtime_trending_searches_df = pytrends.realtime_trending_searches(pn='US')
 
-    # Get top charts for a specific year
-    top_charts_df = pytrends.top_charts(2022, hl='en-US', tz=300, geo='US')
+        realtime_trending_searches = extract_arrays(realtime_trending_searches_df.values.flatten().tolist())
+        realtime_trending_searches = [remove_non_english(item) for item in realtime_trending_searches if not isinstance(item, list)]
 
-    # Process the retrieved data
-    top_charts = top_charts_df["title"].tolist()
+        trending_searches = trending_searches_df.values.flatten().tolist()
+        trending_searches2 = trending_searches_df_canada.values.flatten().tolist()
 
-    # Fetch suggestions for trending searches
-    suggested_keywords = []
-    for keyword in trending_searches:
-        suggestions = pytrends.suggestions(keyword=keyword)
-        suggested_keywords.extend(suggestions)
+        suggested_keywords = []
+        futures = [executor.submit(partial(fetch_suggestions, keyword, pytrends)) for keyword in trending_searches]
+        results = [future.result() for future in futures]
+        suggested_keywords.extend([item for sublist in results for item in sublist])
 
-    # Create a DataFrame from the suggested keywords
+        max_iterations = min(30, len(realtime_trending_searches))
+        futures = [executor.submit(partial(fetch_suggestions, keyword, pytrends)) for keyword in realtime_trending_searches[:max_iterations]]
+        results = [future.result() for future in futures]
+        suggested_keywords.extend([item for sublist in results for item in sublist])
+
+        pytrends_pool.return_connection(pytrends)
+
     df = pd.DataFrame(suggested_keywords)
+    combined_array = realtime_trending_searches + trending_searches + trending_searches2 + df['title'].tolist()
 
-    # Combine all the search trends and suggested keywords into a single array
-    combined_array = trending_searches + top_charts + df['title'].tolist()
+    random_param = request.args.get('random')
+
+    if random_param is not None:
+        try:
+            n = int(random_param)
+            if n > 0:
+                random.shuffle(combined_array)
+                combined_array = combined_array[:n]
+        except ValueError:
+            return jsonify({"error": "Invalid 'random' parameter value. Must be a positive integer."}), 400
 
     return jsonify(combined_array)
 
